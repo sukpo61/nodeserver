168faab086253e2bd6d76804caa6dd94732abf94
go <sukpo6010@gmail.com>
2023-02-21 11:57:33 +0900

    nodeserver

--- src/server.js
+++ src/server.js
@@ -13,18 +13,22 @@ app.set("views", __dirname + "/src/public/views");
 app.use("/public", express.static(__dirname + "/src/public"));
 //pug 에서 script 접근할때 static 설정을 함
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (_, res) => res.render("home"));
 // 주소가 "/" 일떄 기본디렉토리 + 인자 값을 랜더함 /src/public/views/home 을 랜더하는거지
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handlelisten = () => console.log(`Listening on http://localhost:5000`);
+const handlelisten = () => console.log(`Listening on http://localhost:4500`);
 
 const httpServer = http.createServer(app);
 
-const wsServer = new Server(httpServer);
+const io = new Server(httpServer);
 
 function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+  return io.sockets.adapter.rooms.get(roomName)?.size;
 }
 
 function publicRooms() {
@@ -32,7 +36,7 @@ function publicRooms() {
     sockets: {
       adapter: { sids, rooms },
     },
-  } = wsServer;
+  } = io;
   const publicRooms = [];
   rooms.forEach((_, key) => {
     if (sids.get(key) === undefined) {
@@ -42,36 +46,84 @@ function publicRooms() {
   return publicRooms;
 }
 //public 방을 찾기 위해 개인방이 포함된 방리스트에서 소캣 아이디를 가진 개인 방을 뺌
-const sockets = [];
 
-wsServer.on("connection", (socket) => {
-  wsServer.sockets.emit("room_change", publicRooms());
-  socket["nickname"] = "Anon";
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    // console.log(socket.rooms);
-    //기본적으로 소캣은 id 명의 private 방에 혼자 있으며 방을 생성할 수 있음.
-    done();
-
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-    // 나를 제외한 모두에게 welcome을 실행함. 방인원수 본인한테는 안보임;;
-    wsServer.sockets.emit("room_change", publicRooms());
-    //방 리스트 확인
-    socket.on("disconnecting", () => {
-      socket.rooms.forEach((room) =>
-        socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-      );
-    });
-    socket.on("disconnect", () => {
-      wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("new_message", (msg, room, done) => {
-      socket.to(room).emit("new_message", `${socket.nickname}:${msg}`);
-      done();
-    });
-    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+io.on("connection", (socket) => {
+  socket.on("userid", (userid) => {
+    socket["nickname"] = userid;
   });
-  // console.log(socket);
+
+  const numClients = io.engine.clientsCount;
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome", socket["nickname"]);
+    socket.on("offer", async (offer, offerid, answerid) => {
+      console.log("offer");
+      const usersocket = Array.from(io.sockets.sockets.values()).find(
+        (s) => s.nickname === answerid
+      );
+      if (usersocket) {
+        usersocket.emit("offer", offer, offerid, answerid);
+      } else {
+        console.log("user is not exist");
+      }
+    });
+    console.log("answeron");
+    socket.on("answer", async (answer, offerid, answerid) => {
+      console.log(offerid, answerid);
+      const usersocket = Array.from(io.sockets.sockets.values()).find(
+        (s) => s.nickname === offerid
+      );
+      console.log(usersocket);
+      if (usersocket) {
+        usersocket.emit("answer", answer, answerid);
+      } else {
+        console.log("user is not exist");
+      }
+    });
+
+    socket.on("ice", (ice, targetid) => {
+      if (ice) {
+        socket.to(roomName).emit("ice", ice, targetid);
+      }
+    });
+  });
+
+  socket.on("leave", (targetid, roomName) => {
+    socket.to(roomName).emit("leave", targetid);
+    socket.leave(roomName);
+    const roomSize = io.sockets.adapter.rooms.get(roomName).size;
+    console.log(roomSize);
+  });
 });
 
-httpServer.listen(5000, handlelisten);
+// const OfferMiddle = async (offer, offerid, answerid) => {
+//   // console.log(numClients);
+//   const usersocket = Array.from(io.sockets.sockets.values()).find(
+//     (s) => s.nickname === answerid
+//   );
+//   if (usersocket) {
+//     console.log(usersocket);
+//     usersocket.emit("offer", offer, offerid, answerid);
+//   } else {
+//     console.log("user is not exist");
+//   }
+// };
+
+// const AnswerMiddle = async (answer, offerid, answerid) => {
+//   const usersocket = Array.from(io.sockets.sockets.values()).find(
+//     (s) => s.nickname === offerid
+//   );
+//   if (usersocket) {
+//     usersocket.emit("answer", answer, answerid);
+//   } else {
+//     console.log("user is not exist");
+//   }
+// };
+
+// const IceMiddle = (ice, targetid) => {
+//   if (ice) {
+//     socket.to(roomName).emit("ice", ice, targetid);
+//   }
+// };
+
+httpServer.listen(4500, handlelisten);
